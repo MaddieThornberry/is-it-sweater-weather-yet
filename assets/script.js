92ca3cb9cda1788bc7cb21d361a968f0a5038dcc
maddie <madeleinethornberry@gmail.com>
2022-08-14 14:25:15 -0400

    Added HTML, CSS, JS

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,24 @@
 // card loads
 // local storage
 
-var request = newHttp
+const chosenEl = document.getElementById('chosen')
+const cardTempEl = document.getElementById('temp')
 
-request.open('GET', '')
+
+
+
+
+
+
+
+//var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={8458f7c3f7570fa3d32d9f565428b8a3}'
+
+//var xhr = new XMLHttpRequest(); 
+//xhr.onreadystatchange = function (){
+   // if (xhr.readyState === XMLHttpRequest.DONE) {
+    //    console.log('XMLHttpRequest Response \n-------------');
+    //    console.log(xhr.response);
+    //}
+//};
+//xhr.open('GET', requestUrl);
+//xhr.send();
